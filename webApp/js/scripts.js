465e12b397965a8ae9a012881a28acc2573238eb
Tais Bellini <tais38@gmail.com>
2016-02-14 00:16:45 -0200

    maker key

--- webApp/js/scripts.js
+++ webApp/js/scripts.js
@@ -1,6 +1,9 @@
 $('#shippoForm').submit(function(event){
 	event.preventDefault();
-	data = {"tracking_number" : $('#tracking').val(), "carrier" :  $('#carrier').val()};
+	var tracking = $('#tracking').val();
+	var carrier = $('#carrier').val();
+	var data = {"tracking_number" : tracking, "carrier" :  carrier};
+	var makerkey = $('#makerkey').val();
 
 	$.ajax({
 		type: "POST",
@@ -10,6 +13,22 @@ $('#shippoForm').submit(function(event){
 			"Content-type" : "application/json"
 		},
 		contentType: 'application/json',
-		data: JSON.stringify(data)	
+		data: JSON.stringify(data),
+		success: function(response){
+			var makerUser = {
+				key = makerkey,
+				tracking_number = tracking
+			};
+
+			var mongoUrl = "https://api.mongolab.com/api/1/databases/ds059375/collections/trackingKey?apiKey=M--cOAyDpsPcwUSEAs8QV_XcLP6mMsaZ"
+
+			$.ajax({
+			type: "POST",
+			url: mongoUrl,
+			data: JSON.stringify(makerUser),
+			contentType: "application/json"
+			});
+		}
 	});
+
 });
